7de0a6b00715a5d2a4f1f043f2e945d923807600
luukbroekhof <luukbroekhof@icloud.com>
2021-09-13 20:26:59 +0200

    Initial commit

---
+++ RaceSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RaceSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
