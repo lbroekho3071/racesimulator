b4759e0d6b262b6e38fcab777eba09830dd556d2
Luuk Broekhof <lbroekho3071@student.landstede.nl>
2021-09-14 16:48:26 +0200

    Participant interface

---
+++ Model/Interfaces/IParticipant.cs
@@ -0,0 +1,10 @@
+namespace Model.Interfaces
+{
+    public interface IParticipant
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public IEquipment Equipment { get; set; }
+        public TeamColors TeamColor { get; set; }
+    }
+}
