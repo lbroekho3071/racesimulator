6b13651801ff708da41696299911059b20d5089f
luukbroekhof <luukbroekhof@icloud.com>
2021-09-15 10:13:50 +0200

    Added Competition.cs file

---
+++ Model/Classes/Competition.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Model.Interfaces;
+
+namespace Model.Classes
+{
+    public class Competition
+    {
+        public List<IParticipant> Participants;
+        public Queue<Track> Tracks;
+
+        // public Track NextTrack()
+        // {
+        //     Tracks.
+        // }
+    }
+}
