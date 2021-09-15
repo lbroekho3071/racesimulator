9d454d7be22c32590e2407ffdcb4d6be51c65607
luukbroekhof <luukbroekhof@icloud.com>
2021-09-15 10:14:02 +0200

    Added Car.cs file

---
+++ Model/Classes/Car.cs
@@ -0,0 +1,12 @@
+﻿using Model.Interfaces;
+
+namespace Model.Classes
+{
+    public class Car : IEquipment
+    {
+        public int Quality { get; set; }
+        public int Performance { get; set; }
+        public int Speed { get; set; }
+        public bool IsBroken { get; set; }
+    }
+}
