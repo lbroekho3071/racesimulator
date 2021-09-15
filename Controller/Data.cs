be8449579544d5275a9bb36a2775fecca394983a
luukbroekhof <luukbroekhof@icloud.com>
2021-09-15 16:48:21 +0200

    Static Data class made

---
+++ Controller/Data.cs
@@ -0,0 +1,14 @@
+﻿using Model.Classes;
+
+namespace Controller
+{
+    public static class Data
+    {
+        public static Competition Competition { get; set; }
+
+        static Data()
+        {
+            Competition = new Competition();
+        }
+    }
+}
