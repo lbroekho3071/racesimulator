ef1ded0eafc60a8475aae86b8c7f45ae91d3aa40
luukbroekhof <luukbroekhof@icloud.com>
2021-09-15 10:13:23 +0200

    Added Driver.cs file

---
+++ Model/Classes/Driver.cs
@@ -0,0 +1,13 @@
+﻿using Model.Enums;
+using Model.Interfaces;
+
+namespace Model.Classes
+{
+    public class Driver : IParticipant
+    {
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public IEquipment Equipment { get; set; }
+        public TeamColors TeamColor { get; set; }
+    }
+}
