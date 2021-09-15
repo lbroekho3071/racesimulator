b37922302ea962fdcef9bdc92a39f2c06e7ae818
luukbroekhof <luukbroekhof@icloud.com>
2021-09-15 19:12:36 +0200

    Simulation start

--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading;
+using Controller;
 
 namespace Simulator
 {
@@ -6,7 +8,14 @@ namespace Simulator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Data.Initialize();
+            Data.NextRace();
+            Console.WriteLine(Data.CurrentRace.Track.Name);
+
+            for (;;)
+            {
+                Thread.Sleep(100);
+            }
         }
     }
 }
