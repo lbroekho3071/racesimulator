f8d6c0d92ac325eb7b2124bfb07b96e8d18a0f22
luukbroekhof <luukbroekhof@icloud.com>
2021-09-15 16:34:03 +0200

    SectionData.cs file

---
+++ Model/Classes/SectionData.cs
@@ -0,0 +1,12 @@
+﻿using Model.Interfaces;
+
+namespace Model.Classes
+{
+    public class SectionData
+    {
+        public IParticipant Left { get; set; }
+        public int DistanceLeft { get; set; }
+        public IParticipant Right { get; set; }
+        public int DistanceRight { get; set; }
+    }
+}
