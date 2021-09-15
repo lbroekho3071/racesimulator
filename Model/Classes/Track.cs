a11031a59c26842428f70c5e82a3e7528a889cb9
luukbroekhof <luukbroekhof@icloud.com>
2021-09-15 10:13:39 +0200

    Added Track.cs file

---
+++ Model/Classes/Track.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Model.Enums;
+
+namespace Model.Classes
+{
+    public class Track
+    {
+        public string Name;
+        public LinkedList<Section> Sections;
+
+        public Track(string name, Section[] sections)
+        {
+            Name = name;
+            foreach (Section section in sections)
+            {
+                Sections.AddLast(section);
+            }
+        }
+    }
+}
