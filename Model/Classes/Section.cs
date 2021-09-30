064865171619b61ed69b5920dc043134090b4667
luukbroekhof <luukbroekhof@icloud.com>
2021-09-30 14:11:48 +0200

    Added Position and Direction variables to Section.cs

--- Model/Classes/Section.cs
+++ Model/Classes/Section.cs
@@ -1,10 +1,13 @@
-﻿using Model.Enums;
+﻿using System.Drawing;
+using Model.Enums;
 
 namespace Model.Classes
 {
     public class Section
     {
         public SectionTypes SectionType;
+        public Point Position { get; set; }
+        public int Direction { get; set; }
 
         public Section(SectionTypes sectionType)
         {
