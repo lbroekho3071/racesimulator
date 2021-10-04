99380485a5d0714a3ff4e0f193c290331fa41a2c
luukbroekhof <luukbroekhof@icloud.com>
2021-10-04 15:34:36 +0200

    Method to get and set the start positions of the participants

--- Controller/Race.cs
+++ Controller/Race.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Model.Classes;
+using Model.Enums;
 using Model.Interfaces;
 
 namespace Controller
@@ -17,6 +19,27 @@ namespace Controller
         {
             Track = track;
             Participants = participants;
+        }
+
+        public void SetStartingPosition()
+        {
+            List<Section> sections =
+                Track.Sections.Where((item) => item.SectionType == SectionTypes.StartGrid).Reverse().ToList();
+
+            for (int i = 0; i < Participants.Count; i++)
+            {
+                Section section = sections.ElementAt(i / 2);
+                SectionData data = GetSectionData(section);
+
+                if (data.Left == null)
+                {
+                    data.Left = Participants[i];
+                }
+                else
+                {
+                    data.Right = Participants[i];
+                }
+            }
         }
 
         public SectionData GetSectionData(Section section)
