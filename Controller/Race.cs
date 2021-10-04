97ea96877cbf863cd05b4406d2012b9cc85b1e3f
luukbroekhof <luukbroekhof@icloud.com>
2021-10-04 15:35:06 +0200

    Forgot to call the previous made method

--- Controller/Race.cs
+++ Controller/Race.cs
@@ -19,6 +19,8 @@ namespace Controller
         {
             Track = track;
             Participants = participants;
+            
+            SetStartingPosition();
         }
 
         public void SetStartingPosition()
