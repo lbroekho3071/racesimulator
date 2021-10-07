04b51ff9d475a54612605e8b114d25beb9fb98f6
luukbroekhof <luukbroekhof@icloud.com>
2021-10-07 14:34:30 +0200

    Added constructor to DriversChangedEventArgs.cs to add the Track

--- Model/Classes/DriversChangedEventArgs.cs
+++ Model/Classes/DriversChangedEventArgs.cs
@@ -5,5 +5,10 @@ namespace Model.Classes
     public class DriversChangedEventArgs : EventArgs
     {
         public Track Track { get; set; }
+
+        public DriversChangedEventArgs(Track track)
+        {
+            this.Track = track;
+        }
     }
 }
